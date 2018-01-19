c9e21e062401ff42d493e10e1d4abe0976f0e6dc
nathanknautz <nate.knautz@gmail.com>
2018-01-18 21:36:24 -0600

    updated with routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
- get "/all_products_url" => "products#products_all_method"
+ get "/products" => "products#index"
+ post "/products" => 'products#create'
+ get "/products/:id" => "products#show"
+ patch "/products/:id" => "products#update"
+ delete "/products/:id" => 'products#destroy'
 end
