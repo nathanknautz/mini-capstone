e6432bf3d15b35fb57a359d00f8bd31a486c1b78
nathanknautz <nate.knautz@gmail.com>
2018-01-21 15:04:46 -0600

    updated with model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+
+  def discounted?
+    price < 1000
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+  def as_json
+    {
+      name: name,
+      price: price,
+      image_url: image_url,
+      description: description,
+      discounted: discounted?,
+      tax: tax,
+      total: total
+    }
+  end
 end
