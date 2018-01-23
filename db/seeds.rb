a5655ca2c7a3d06b52bb4b3beb8f3d311440ea12
nathanknautz <nate.knautz@gmail.com>
2018-01-22 21:22:15 -0600

    added info to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
-
+Product.create!([
+  {name: "Cairn Terrier", price: "500.0", image_url: "http://www.petguide.com/wp-content/uploads/2013/02/cairn-terrier-21.jpg", description: "The Cairn Terrier is one of the oldest terrier breeds, originating in the Scottish Highlands and recognized as one of Scotland's earliest working dogs.", in_stock: false},
+  {name: "Golden Retriever", price: "1500.0", image_url: "http://cdn1-www.dogtime.com/assets/uploads/gallery/golden-retriever-dogs-and-puppies/golden-retriever-dogs-puppies-9.jpg", description: "The Golden Retriever is a large-sized breed of dog bred as gun dogs to retrieve shot waterfowl.", in_stock: false},
+  {name: "Dachshund", price: "1000.0", image_url: "http://celebritydogwatcher.com/wp-content/uploads/2017/09/dachshund_names_german_food.jpg", description: "it has short legs and a long body. resembles a hot dog...", in_stock: false},
+  {name: "Pug", price: "800.0", image_url: "http://cdn2-www.dogtime.com/assets/uploads/gallery/pug-dog-breed-pictures/2-face.jpg", description: "A lot of dog, in a small place", in_stock: false},
+  {name: "Golden Doodle", price: "1999.0", image_url: "www.google.com/golden_doodle", description: "a cross between a poodle and the much better golden retriever", in_stock: false},
+  {name: "German Shepherd", price: "1000.0", image_url: "\"\"", description: "The German Shepherd is a breed of medium to large-sized working dog that originated in Germany.", in_stock: false}
+])
